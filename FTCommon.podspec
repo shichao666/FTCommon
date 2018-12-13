f543aab5f43a6e75e4ee1e39b37f66bfbb81dea5
shichao <329585013@qq.com>
2018-12-13 17:21:43 +0800

    提交

--- FTCommon.podspec
+++ FTCommon.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s| 
 s.name = "FTCommon" 
-s.version = "1.1.20"
+s.version = "1.1.21"
 s.summary = "福田iOS-APP公共模块 第一版"
 s.description = "福田iOS-APP公共模块 第一版."
 s.homepage = "https://github.com/shichao666/FTCommon" 
@@ -12,7 +12,7 @@ s.source = { :git => "https://github.com/shichao666/FTCommon.git", :tag => s.ver
 
 # /*.{h,m}
 # /**/*
-s.source_files = "FTCommon/**/*"
+s.source_files = "FTCommon/*"
 
 # s.subspec 'FTCategory' do |category|
 # category.source_files = "FTCommon/Category/**/*"
---
+++ FTCommon/FTCommon.framework/Headers/FTCommon.h
@@ -0,0 +1,19 @@
+//
+//  FTCommon.h
+//  FTCommon
+//
+//  Created by 史超 on 2018/12/13.
+//  Copyright © 2018年 史超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FTCommon.
+FOUNDATION_EXPORT double FTCommonVersionNumber;
+
+//! Project version string for FTCommon.
+FOUNDATION_EXPORT const unsigned char FTCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FTCommon/PublicHeader.h>
+
+#import <FTCommon/FTCommonMacro.h>
